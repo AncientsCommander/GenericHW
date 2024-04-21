ae3fb08df92454b339a40ea1386e8bb834a31741
Ancient <atlantis159@mail.ru>
2024-04-21 18:06:07 +0500

    Initial Commit

---
+++ Generic/ComparablePair.cs
@@ -0,0 +1,19 @@
+﻿namespace Generic;
+
+public class ComparablePair<TFirst, TSecond> : IComparable<ComparablePair<TFirst, TSecond>> where TFirst : IComparable<TFirst> where TSecond : IComparable<TSecond>
+{
+    public TFirst First { get; set; }
+    public TSecond Second { get; set; }
+
+    public ComparablePair(TFirst first, TSecond second)
+    {
+        First = first;
+        Second = second;
+    }
+
+    public int CompareTo(ComparablePair<TFirst, TSecond> input)
+    {
+        var firstComparison = First.CompareTo(input.First);
+        return firstComparison == 0 ? Second.CompareTo(input.Second) : firstComparison;
+    }
+}
---
+++ Generic/Pair.cs
@@ -0,0 +1,13 @@
+﻿namespace Generic;
+
+public class Pair<TFirst,TSecond>
+{
+    public TFirst FirstType { get; set; }
+    public TSecond SecondType { get; set; }
+
+    public Pair(TFirst firstType, TSecond secondType)
+    {
+        FirstType = firstType;
+        SecondType = secondType;
+    }
+}
